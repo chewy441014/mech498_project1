47b6ca0b6c346654b3b4be33108a5bed42a497fb
chewy441014 <p.hill.ultimate@gmail.com>
2016-04-07 23:16:44 -0500

    finished DunkTrajectory
    only changes angle five, for a nice sweeping dunk!

--- createDunkTrajectory.m
+++ createDunkTrajectory.m
@@ -1,3 +1,8 @@
-function dunk_trajectory = createDunkTrajectory(robot)
-
+function dunk_trajectory = createDunkTrajectory(joint_angles)
+    if round(joint_angles(5)) ~= pi
+        disp('The robots fifth joint is not in the correct position.')
+    end
+    len = 100;
+    dunk_trajectory = ones(1,len)*joint_angles;
+    dunk_trajectory(5,:) = linspace(0,pi,len) + dunk_trajectory(5,:);
 end
