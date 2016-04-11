da36ca9349e28ed12b94108964fa51bdb0903e0a
ikt1 <IAN@Alan-Tomkinsons-iMac.local>
2016-04-11 18:00:15 -0500

    minor changes in 401 work

--- z401ManipulatorDesign.m
+++ z401ManipulatorDesign.m
@@ -1,4 +1,6 @@
-function z401ManipulatorDesign(robot)
+function z401ManipulatorDesign()
+
+robot = basketInit();
 
 % robot = robotInit array
 
@@ -31,23 +33,46 @@ l4 = robot.parameters.l_4;
 joint_angles = [0;0;0;0;0];
 joint_vel = [0;0;0;0;0];
 [~, ~, G] = basketDynamics(joint_angles, joint_vel, robot);
-tau = G;
+tau = G
 
 % Member 4
-F5 = m4*g + m5*g;
-x4 = [0, l4/2, l4];
-shear4 = [F5, F5-m4*g, 0];
-moment4 = [tau(5), tau(5)+F5*l4/2, 0];
 
-stairs(x4,shear4);
-title('Member 4 Shear Diagram');
+%George's inferior code
+
+% F5 = m4*g + m5*g;
+% x4 = [0, l4/2, l4];
+% shear4 = [F5, F5-m4*g, 0];
+% moment4 = [tau(5), tau(5)+F5*l4/2, 0];
+% 
+% stairs(x4,shear4);
+% title('Member 4 Shear Diagram');
+% xlabel('x (m)');
+% ylabel('Shear (N)');
+% 
+% figure
+% plot(x4,moment4);
+% title('Member 4 Moment Diagram');
+% xlabel('x (m)');
+% ylabel('Moment (N-m)');
+
+%Treating weight as a distributed load
+
+x=linspace(0,l4,100);
+w=m4*g/l4;
+V=w*(l4-x);
+M=-w/2*(l4-x).^2;
+%Ymax=(-wl4^4)/(8EI)
+
+plot(x,V)
 xlabel('x (m)');
 ylabel('Shear (N)');
 
-plot(x4,moment4);
-title('Member 4 Moment Diagram');
+figure
+plot(x,M)
 xlabel('x (m)');
 ylabel('Moment (N-m)');
+
+%Member 3
 
 
 
