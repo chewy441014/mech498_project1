7b71e8f4bc3a7642e6d16532a8594222362c365d
Ameliabqy <bianqingyun1995@gmail.com>
2016-04-15 14:03:00 -0500

    implement dunking in simulateBasket

--- Dunking.m
+++ Dunking.m
@@ -1,4 +1,4 @@
-function [Dunking_trajectory] = Dunking(init_angles, dt)
+function [joint_angles_mat, time] = Dunking(init_angles, dt)
 robot = basketInit;
 theta_ref1 = [pi/2; 0.35; -0.8; 0; pi/2];
 theta_ref1 = [theta_ref1 zeros(5,1)];
@@ -10,7 +10,7 @@ distance = norm(abs(init_pos - pos_ref));
 avg_vel = 1;
 t_f = distance/avg_vel;
 time1 = 0:dt:t_f;
-K_p = 20*ones(1, 5);
+K_p = 100*ones(1, 5);
 K_v = 10*ones(1, 5);
 init_angles = [init_angles zeros(5,1)];
 disp('        ')
@@ -18,8 +18,6 @@ disp('Calculating trajectory to pre-dunking position')
 disp('        ')
 [joint_angles_mat1, joint_velocities_mat] = ...
     controlBasketPID(init_angles, theta_ref1, K_p, K_v, time1, robot);
-robot.handles = drawBasket(robot.home_angles, [0,0,0], robot);
-
 %throwing the ball
 theta_ref2 = [pi/2; 0.15; -0.6; 0; 0];
 theta_dot_ref = [0; 0; 0; 0; -3];
@@ -30,7 +28,7 @@ t_f = (theta_ref2(end) - theta_ref1(end))/(theta_dot_ref(end));
 time2 = 0:dt:t_f;
 K_p = 150*ones(1, 5);
 K_v = 20*ones(1, 5);
-init_angles = [theta_ref1 zeros(5,1)];
+init_angles = [joint_angles_mat1(:, end) zeros(5,1)];
 disp('      ')
 disp('Calculating dunking trajectory')
 disp('        ')
@@ -39,13 +37,6 @@ disp('        ')
 disp('       ')
 time = [time1 time2];
 frames_per_second = 60;
-number_of_frames = time(end)*frames_per_second;
-skip_frames = round(length(time)/number_of_frames);
 joint_angles_mat = [joint_angles_mat1 joint_angles_mat2];
-robot.handles = drawBasket(robot.home_angles, [0,0,0], robot);
-pause
-for t = 1:skip_frames:length(time)
-    setBasket(joint_angles_mat(:,t), robot);
-end
 
 end
--- simulateBasket.m
+++ simulateBasket.m
@@ -8,7 +8,7 @@ robot = basketInit();
 if is == false
     fprintf('\n')
     disp('Correct the Ball Trajectory')
-    pause(1);
+
 end
 home_pos = robot.home_pos;
 home_angles = robot.home_angles;
@@ -57,7 +57,6 @@ K_p = 20*ones(5,1);
 K_v = 20*ones(5,1);
 disp('Control Basket PID')
 disp('      ')
-pause(0.5)
 [joint_angles_mat1,~] = controlBasketPID(theta_init, theta_ref,  K_p, K_v, time1, robot);
 end_angles = joint_angles_mat1(:,end);
 
@@ -76,7 +75,6 @@ Kv = 25*ones(5,1);
 fprintf('\n')
 disp('Control Basket Impulse')
 disp('      ')
-pause(0.5)
 [joint_angles_mat2, ~] = controlBasketImpulse(theta_init2, Kp, Kv, robot, tangent, time2);
 end_angles2 = joint_angles_mat2(:,end);
 
@@ -112,14 +110,12 @@ K_v2 = 20*ones(5,1);
 fprintf('\n')
 disp('Control Basket PID')
 disp('      ')
-pause(0.5)
 [joint_angles_mat3,~] = controlBasketPID(theta_init2, theta_ref, K_p2, K_v2, time3, robot);
 end_angles3 = joint_angles_mat3(:,end);
 
 fprintf('\n')
 disp('Calculating new Ball Pos ')
 disp('      ')
-pause(0.5)
 
 number_of_frames3 = time3(end)*frames_per_second;
 skip_frames3 = round(length(time3)/number_of_frames3);
@@ -140,23 +136,14 @@ prompt = 'Run the dunking code? (Type int only, 1 = true, 0 = false) ';
 x = input(prompt);
 
 if x
-    K_p3 = 6*ones(5,1);
-    K_v3 = 10*ones(5,1);
-    
-    dunk_trajectory = createDunkTrajectory(end_angles3,dt,5*t_f);
-    theta_init = [end_angles3, zeros(5,1)];
-    time4 = 0:dt:4*t_f;
     
     fprintf('\n')
     disp('Control Dunk PID')
     disp('      ')
-    pause(0.5)
-    [joint_angles_mat4] = controlDunkPID(theta_init, dunk_trajectory, K_p3, K_v3, time4, robot);
-    
+    [joint_angles_mat4, time4] = Dunking(end_angles3, dt);
     fprintf('\n')
     disp('Calculating new Ball Pos ')
     disp('      ')
-    pause(0.5)
     
     number_of_frames4 = time4(end)*frames_per_second;
     skip_frames4 = round(length(time4)/number_of_frames4);
